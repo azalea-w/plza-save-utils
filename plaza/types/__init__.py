5a48fdb3ab5d05b1d3dcee160256115348026cb6
azalea <238357242+azalea-w@users.noreply.github.com>
2025-10-21 20:08:26 +0200

    fix: add DressUp to types init

--- plaza/types/__init__.py
+++ plaza/types/__init__.py
@@ -1,4 +1,6 @@
-from .bagsave import BagSave, BagEntry, BagFlagID, BagReleaseCategory, CategoryType
-from .coredata import CoreData, UserDataSaveDataAccessor
-from .pokedex import PokedexSaveDataAccessor, PokedexData, PokedexCoreData, PokedexKind, DrawData
 from .accessors import HashDBKeys
+from .bagsave import BagEntry, BagFlagID, BagReleaseCategory, BagSave, CategoryType
+from .coredata import CoreData, UserDataSaveDataAccessor
+from .playerdata import CopyDressUpSaveData, DRESSUP_CATEGORY_NAMES, DressupCategory, PlayerSaveDataAccessor, \
+    STYLE_CATEGORY_NAMES, StyleCategory
+from .pokedex import DrawData, PokedexCoreData, PokedexData, PokedexKind, PokedexSaveDataAccessor
